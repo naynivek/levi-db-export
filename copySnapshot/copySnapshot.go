944b04a6002cb2c3101d71c7fc5fabf50b68b4f3
Kevin Yan <naynivek@amazon.com>
2023-12-05 16:19:06 -0300

    add KMSKeyId option

--- copySnapshot/copySnapshot.go
+++ copySnapshot/copySnapshot.go
@@ -7,11 +7,12 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/rds"
 )
 
-func CopySnapshot(rdsClient *rds.Client, SourceSnapshotArn,TargetSnapshotName string) (*rds.CopyDBSnapshotOutput, error) {
+func CopySnapshot(rdsClient *rds.Client, SourceSnapshotArn,TargetSnapshotName,KmsKeyId string) (*rds.CopyDBSnapshotOutput, error) {
 	output, err := rdsClient.CopyDBSnapshot(context.TODO(),
 		&rds.CopyDBSnapshotInput{
 			SourceDBSnapshotIdentifier: aws.String(SourceSnapshotArn),
 			TargetDBSnapshotIdentifier: aws.String(TargetSnapshotName),
+			KmsKeyId: aws.String(KmsKeyId),
 		})
 	if err != nil {
 		log.Printf("Couldn't create the task %v: %v\n", SourceSnapshotArn, err)
